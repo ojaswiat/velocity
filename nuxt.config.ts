5df780486cb2ab519eed6285f7e51c8216f85d8f
Ojaswi Athghara <ojaswiat@gmail.com>
2025-10-22 14:40:15 +0530

    auto-import types

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -23,6 +23,7 @@ export default defineNuxtConfig({
             resolve("./app/middleware/**"),
             resolve("./app/services/**"),
             resolve("./app/stores/**"),
+            resolve("./app/types/**"),
             resolve("./app/utils/**"),
         ],
     },
